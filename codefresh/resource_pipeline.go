cc51c88caee398278098f0410238d91224b45066
mattmelgard <matt.j.melgard@gmail.com>
2021-04-28 13:14:56 -0600

    Add support for packId in codefresh pipeline

--- codefresh/resource_pipeline.go
+++ codefresh/resource_pipeline.go
@@ -79,6 +79,10 @@ func resourcePipeline() *schema.Resource {
 							Optional: true,
 							Default:  0, // zero is unlimited
 						},
+						"pack_id": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
 						"spec_template": {
 							Type:     schema.TypeList,
 							Optional: true,
@@ -563,6 +567,7 @@ func mapResourceToPipeline(d *schema.ResourceData) *cfClient.Pipeline {
 			Concurrency:        d.Get("spec.0.concurrency").(int),
 			BranchConcurrency:  d.Get("spec.0.branch_concurrency").(int),
 			TriggerConcurrency: d.Get("spec.0.trigger_concurrency").(int),
+			PackId:             d.Get("spec.0.pack_id").(string),
 		},
 	}
 
